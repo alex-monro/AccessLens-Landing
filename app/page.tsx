5f2168e3c5e4ef601fb0375da3c64a77d670220c
MIKE <187060323+mtq01@users.noreply.github.com>
2026-05-02 01:00:33 -0700

    Skeleton Complete

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,21 @@ import Hero from "@/components/Hero";
 import VideoSection from "@/components/VideoSection";
 import CardsSection from "@/components/CardsSection";
 import DownloadSection from "@/components/DownloadSection";
+import StartSection from "@/components/StartSection";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Navbar />
       <Hero />
+      <main className="max-w-6xl mx-auto px-6">
       <VideoSection />
       <CardsSection />
       <DownloadSection />
+      <StartSection />
+      </main>
       <Footer />
-    </main>
+    </>
   );
 }
--- components/DownloadSection.tsx
+++ components/DownloadSection.tsx
@@ -1,7 +1,15 @@
 const DownloadSection = () => {
   return (
-    <section className="download-section">DownloadSection</section>
-  )
-}
+    <section className="flex flex-col items-start gap-4 py-12">
+      <h2 className="text-2xl lg:text-3xl font-bold">No Free Tool Can Catch Everything</h2>
+      <p className="text-md lg:text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        quisquam perferendis ab debitis ducimus vel? Natus aliquam rerum odio
+        illo.
+      </p>
+      <button className="cursor-pointer">Book an Audit</button>
+    </section>
+  );
+};
 
-export default DownloadSection
+export default DownloadSection;
